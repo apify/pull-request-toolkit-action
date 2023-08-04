e6cbfcd89ad510b6360b26181c4ab960c2e5f882
Marek Trunkát <marek@trunkat.eu>
2023-08-04 09:56:25 +0200

    Test

--- dist/index.js
+++ dist/index.js
@@ -380,14 +380,12 @@ const github = __importStar(__nccwpck_require__(5438));
 const helpers_1 = __nccwpck_require__(5008);
 const consts_1 = __nccwpck_require__(4831);
 async function run() {
-    var _a, _b, _c, _d, _e;
+    var _a, _b;
     try {
-        console.log((_a = github.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.base);
-        console.log((_b = github.context.payload.pull_request) === null || _b === void 0 ? void 0 : _b.base.repo);
-        console.log((_c = github.context.payload.pull_request) === null || _c === void 0 ? void 0 : _c.head);
+        console.log(github.context.payload.pull_request);
         // This disables skips this action when run on a PR from external fork, i.e., when the fork is not a part of the organization.
-        if (!((_d = github.context.payload.pull_request) === null || _d === void 0 ? void 0 : _d.base.repo.full_name.startsWith(`${consts_1.ORGANIZATION}/`))) {
-            core.warning(`Skipping toolkit action for PR from external fork: ${(_e = github.context.payload.pull_request) === null || _e === void 0 ? void 0 : _e.base.repo.full_name}`);
+        if (!((_a = github.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.base.repo.full_name.startsWith(`${consts_1.ORGANIZATION}/`))) {
+            core.warning(`Skipping toolkit action for PR from external fork: ${(_b = github.context.payload.pull_request) === null || _b === void 0 ? void 0 : _b.base.repo.full_name}`);
             return;
         }
         core.info('Pull request is from apify organization, not from an external fork.');
