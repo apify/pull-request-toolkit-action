0db0a1bb9f3a45dd0a4156e58f17423c6a53708e
Marek Trunkát <marek@trunkat.eu>
2023-09-05 14:52:05 +0200

    feat: Speeding up retries in estimation and linking check (#31)
    Co-authored-by: Jiří Moravčík <jiri.moravcik@gmail.com>

--- src/helpers.ts
+++ src/helpers.ts
@@ -396,3 +396,22 @@ export async function isPullRequestTested(octokit: OctokitType, pullRequest: Pul
 
     return testFilePaths.length > 0;
 };
+
+/**
+ * Retries given function `retries` times with `delayMillis` delay between each attempt if the function fails.
+ */
+export async function retry(func: () => Promise<void>, retries: number, delayMillis: number): Promise<void> {
+    let currentRetry = 0;
+    while (true) {
+        try {
+            return await func();
+        } catch (err) {
+            if (currentRetry === retries) throw err;
+
+            core.info(`An attempt has failed. Retrying in ${delayMillis / 1000} secs...`);
+            if (err instanceof Error) console.error(err.message); // eslint-disable-line no-console
+            await new Promise((resolve) => setTimeout(resolve, delayMillis));
+            currentRetry++;
+        }
+    }
+};
